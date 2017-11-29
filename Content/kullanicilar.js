4c08cc8fb7745a51ab9c40e906c1c83f3e3b2665
BEM <BEM@DESKTOP-S3O5AOR>
2017-11-29 11:17:12 +0300

    asd

---
+++ Content/kullanicilar.js
@@ -0,0 +1,27 @@
+﻿[
+    {
+        "kullanici_adi":"te.ori",
+        "sifre": "123456",
+        "isim": "Orhan",
+        
+       "soyisim": "Aygün",
+       "profil_resmi": "img/orhan.jpg",
+       "d_tarihi": "01.01.1985",
+       "mezun_oldugu_okul": "Gazi Üni",
+       "fakulte": "ESEF",
+       "ozgecmis": "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",
+       "konular":["c#","javascript","html"]
+    }, 
+    {  
+       "kullanici_adi": "burak",
+       "sifre": "123456",
+       "isim": "Burak",
+       "soyisim": "Kayhan",
+       "profil_resmi": "img/burak.jpg",
+       "d_tarihi": "30.08.1992",
+       "mezun_oldugu_okul": "NAU",
+       "fakulte": "FLA",
+       "ozgecmis":"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velitNeque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velitNeque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velitNeque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velitNeque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velitNeque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velitNeque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velitNeque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",
+       "konular": ["c#", "javascript", "html,css"]
+    }
+]
